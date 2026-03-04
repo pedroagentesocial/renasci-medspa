f9369cc22132434580906441f6f81a9e8700afd1
pedroagentesocial <pedro@agentesocial.com>
2026-03-04 10:50:04 -0600

    Set production domain to renascimedspa.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pedroagentesocial.github.io',
+  site: 'https://renascimedspa.com',
   integrations: [
     react(), 
     tailwind(), 
